3fd5a4722c54b1c5d94854843af780e73aa7c3f6
Knivier <agnivag.16@gmail.com>
2025-01-15 10:19:47 -0500

    committing stuff

--- demo.py
+++ demo.py
@@ -4,11 +4,12 @@
 @staticmethod
 # Define the main function: this is the entry point of the script
 def main():
-    pass
+    intakeInfo() # Call the intakeInfo function
+
 @staticmethod
 def intakeInfo():
-    age = input("What is your age? ")
-    name = input("What is your name? ")
+    age = input("What year were you born? ")
+    name = input("What do you like to be called? ")
     return "Hello, " + name + "! You are " + age + " years old."
 
 main() # Call the main function
